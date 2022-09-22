106e3cf9b64d7c4f2327a66d1d3d1a03da3d6827
moralespetit <moralespetit@gmail.com>
2022-09-22 20:42:50 +0200

    no message

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 # Enable WhiteNoise's GZip compression of static assets.
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Test Runner Config
